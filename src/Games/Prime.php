cac3111ab34e0233a20ec8ef0b394940a076c8d9
ola-9 <nepogodda@gmail.com>
2023-10-29 16:16:27 +0300

    fix prime

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -10,11 +10,11 @@ const DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 function isPrime(int $number)
 {
-    if ($number < 1) {
+    if ($number < 2) {
         return false;
     }
 
-    for ($i = 2; $i < $number; $i += 1) {
+    for ($i = 2; $i <= sqrt($number); $i++) {
         if ($number % $i === 0) {
             return false;
         }
